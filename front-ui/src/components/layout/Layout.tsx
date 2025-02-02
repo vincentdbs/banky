3dcf063c6c49ddde81c99eeb19c71d6d6de81963
Vincent Dubois <vdubois@coreoz.com>
2025-02-02 10:51:56 +0100

    Add Sidebar

--- front-ui/src/components/layout/Layout.tsx
+++ front-ui/src/components/layout/Layout.tsx
@@ -1,6 +1,13 @@
 import GlobalErrorBoundary from '@components/theme/GlobalErrorBoundary';
+import {
+  Sidebar, SidebarContent,
+  SidebarFooter, SidebarGroup,
+  SidebarGroupContent, SidebarGroupLabel, SidebarHeader,
+  SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarProvider, SidebarTrigger,
+} from '@lib/shadcn/sidebar';
 import React, { ReactNode } from 'react';
 import { ScrollRestoration } from 'react-router-dom';
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
 
 type Props = {
   children: ReactNode,
@@ -9,7 +16,33 @@ type Props = {
 export default function Layout({ children }: Props) {
   return (
     <GlobalErrorBoundary>
-      {children}
+      <SidebarProvider>
+        <Sidebar>
+          <SidebarHeader />
+          <SidebarContent>
+            <SidebarGroup>
+              <SidebarGroupLabel>Application</SidebarGroupLabel>
+              <SidebarGroupContent>
+                <SidebarMenu>
+                  <SidebarMenuItem>
+                    <SidebarMenuButton asChild>
+                      <a>
+                        <Calendar />
+                        <span>Test</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                </SidebarMenu>
+              </SidebarGroupContent>
+            </SidebarGroup>
+          </SidebarContent>
+          <SidebarFooter />
+        </Sidebar>
+        <main>
+          <SidebarTrigger />
+          {children}
+        </main>
+      </SidebarProvider>
       <ScrollRestoration />
     </GlobalErrorBoundary>
   );
--- front-ui/vite.config.ts
+++ front-ui/vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
     proxy: {
       '/api': 'http://localhost:8080',
     },
+    port: 3000,
   },
   resolve: {
     alias: {
