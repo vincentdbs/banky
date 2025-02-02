24f458309b3f8fcb65e06884f19e9adf10246497
Vincent Dubois <vdubois@coreoz.com>
2025-02-02 10:13:44 +0100

    Add tailwind 3

--- front-ui/vite.config.ts
+++ front-ui/vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
+      "@": path.resolve(__dirname, "./src"),
       '@api': path.resolve(__dirname, 'ts-built/api'),
       '@components': path.resolve(__dirname, 'ts-built/components'),
       '@i18n': path.resolve(__dirname, 'ts-built/i18n'),
